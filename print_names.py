f053b056ffd8cfb9f264a5070de2a98f61c2230e
Nurzat Turganbek <122099167+NurzatGitHub@users.noreply.github.com>
2025-02-20 19:06:50 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,1 +1,8 @@
-print("Nurzat Turganbek")
+import sys
+
+# Проверяем, передан ли аргумент
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+    print(f"Hello, {name}!")
+else:
+    print("No name provided!")
